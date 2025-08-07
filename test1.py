f6b59197442d77bd9e1a41c68d24300a85929ba8
Your Name <you@example.com>
2025-08-07 13:59:35 +1000

    WIP: test1

---
+++ test1.py
@@ -0,0 +1,33 @@
+import torch
+
+
+# Suppose we have the following 3 training examples,
+# which may represent token IDs in a LLM context
+idx = torch.tensor([2, 3, 1])
+
+# The number of rows in the embedding matrix can be determined
+# by obtaining the largest token ID + 1.
+# If the highest token ID is 3, then we want 4 rows, for the possible
+# token IDs 0, 1, 2, 3
+num_idx = max(idx)+1 # 4 row
+
+# The desired embedding dimension is a hyperparameter
+out_dim = 5 # 5 col
+
+
+
+
+# We use the random seed for reproducibility since
+# weights in the embedding layer are initialized with
+# small random values
+torch.manual_seed(123)
+
+# row and col
+embedding = torch.nn.Embedding(num_idx, out_dim)
+
+# tensor is training data, feed to embed, will get vector
+# tensor([[ 1.3010,  1.2753, -0.2010, -0.1606, -0.4015]], grad_fn=<EmbeddingBackward0>)
+# 1 row and 5 col
+out = embedding(torch.tensor([1]))
+
+print(out)
