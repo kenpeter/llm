16f68cd8fd88dec077d8691e92bc5dae048d2568
Your Name <you@example.com>
2025-08-07 23:01:37 +1000

    WIP: update

--- dataloader.py
+++ dataloader.py
@@ -55,7 +55,7 @@ class GPTDatasetV1(Dataset):
 def create_dataloader_v1(
     # txt
     txt,
-    # batch size 4
+    # why call batch size (row), because finish one batch then another
     batch_size=4,
     # max len 256
     max_length=256,
@@ -73,13 +73,14 @@ def create_dataloader_v1(
     tokenizer = tiktoken.get_encoding("gpt2")
 
     # slide win, so we have input arr, and target arr
+    # max_len is the max arr length (4)
     dataset = GPTDatasetV1(txt, tokenizer, max_length, stride)
 
     # data loader use the internal methods from GPTDatasetV1
     dataloader = DataLoader(
         # dataset is input_ids and target_ids
         dataset,
-        # batch size 
+        # batch size (8)
         batch_size=batch_size,
         # shuffle
         shuffle=shuffle,
@@ -93,10 +94,55 @@ def create_dataloader_v1(
 
 
 # max len control how many token in single arr
-dataloader = create_dataloader_v1(raw_text, batch_size=8, max_length=4, stride=4, shuffle=False)
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=8, max_length=4, stride=4, shuffle=False
+)
 
 # create iterator from data loader
 data_iter = iter(dataloader)
 # then next get first batch
 first_batch = next(data_iter)
-print(first_batch)
+# print(first_batch)
+
+
+# ============= position embedding ================
+vocab_size = 50257
+output_dim = 256
+
+# 50257 x 256
+token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
+# print(token_embedding_layer)
+
+
+max_length = 4
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=8, max_length=max_length, stride=max_length, shuffle=False
+)
+data_iter = iter(dataloader)
+inputs, targets = next(data_iter)
+
+# print("Token IDs:\n", inputs)
+# print("\nInputs shape:\n", inputs.shape)
+
+# inputs: 8x4
+token_embeddings = token_embedding_layer(inputs)
+# print(token_embeddings.shape)
+# print(token_embeddings)
+
+
+# ======= what are we doing ====
+
+# context leng is max len 4
+context_length = max_length
+# 4 x 256
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+print(pos_embedding_layer)
+print(pos_embedding_layer.weight)
+
+# max len = 4
+# ran tensor([0, 1, 2, 3])
+position_ind = torch.arange(max_length)
+print(position_ind)
+pos_embeddings = pos_embedding_layer(position_ind)
+print(pos_embeddings.shape)
+print(pos_embeddings)
