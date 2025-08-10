9a32e6d77dd85c8ea89abed3597a187d18dfd995
Your Name <you@example.com>
2025-08-11 09:41:45 +1000

    WIP: mask

--- attention.py
+++ attention.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn as nn
 
 inputs = torch.tensor(
     [
@@ -41,12 +42,11 @@ for i, x_i in enumerate(inputs):
 # 6x3 dot 3x6 -> 6x6
 # this retain all input rows
 attn_scores = inputs @ inputs.T
-# print(attn_scores)
 
 
 # attention weight -> 6x6
 attn_weights = torch.softmax(attn_scores, dim=-1)
-# print(attn_weights)
+
 
 # context 6x6 dot 6x3 -> 6x3
 # attention goes 1st means how much attention I need to pay for each input
@@ -81,16 +81,14 @@ tensor([0.4306, 1.4551]) <---- q_2, 1x2
 tensor([0.4433, 1.1419]) <---- k_2
 tensor([0.3951, 1.0037]) <---- v_2
 """
-print(query_2)
-print(key_2)
-print(value_2)
+
 
 # 1. it means other ele in inputs cannot be directly used, but attach with W_key and W_value
 # 2. after this we can use
 keys = inputs @ W_key
 values = inputs @ W_value
 
-print("====")
+
 """
 6x2
 tensor([[0.3669, 0.7646],
@@ -101,7 +99,7 @@ tensor([[0.3669, 0.7646],
         [0.3275, 0.9642]])
 
 """
-print(keys)
+
 """
 6x2
 tensor([[0.1855, 0.8812],
@@ -112,7 +110,7 @@ tensor([[0.1855, 0.8812],
         [0.3221, 0.7863]])
 
 """
-print(values)
+
 
 # the key for 2nd input token
 keys_2 = keys[1]
@@ -122,18 +120,107 @@ attn_score_22 = query_2.dot(keys_2)
 
 # 1x2 @ 6x2.T -> 1x6
 attn_scores_2 = query_2 @ keys.T  # All attention scores for given query
-print(attn_scores_2)
+
 
 
 d_k = keys.shape[1]
 attn_weights_2 = torch.softmax(attn_scores_2 / d_k**0.5, dim=-1)
-print(attn_weights_2)
 
 
-# 1. W_q, W_k, W_val all from inputs @ W_?
+
+# 1. the individual input for the query
 # 2. q_2 = W_q * x_2
 # 3. att_score_2 = q_2 @ all_keys.T
 # 4. att_score_2 -> normalize -> att_weight_2
 # 5. all_context = att_weight_2 @ value (because value is the last one not used.)
 context_vec_2 = attn_weights_2 @ values
-print(context_vec_2)
+
+
+
+# ============
+
+
+# self attention v1
+# nn.Module
+class SelfAttention_v1(nn.Module):
+    # def init
+    # self, d_in, d_out
+    def __init__(self, d_in, d_out):
+        # super init
+        super().__init__()
+        # W_q, W_k, W_val
+        self.W_query = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_key   = nn.Parameter(torch.rand(d_in, d_out))
+        self.W_value = nn.Parameter(torch.rand(d_in, d_out))
+
+    # forward, all inputs x
+    def forward(self, x):
+        # all inputs form key, query, value
+        keys = x @ self.W_key
+        queries = x @ self.W_query
+        values = x @ self.W_value
+        
+        # query @ keys.T -> att score*
+        attn_scores = queries @ keys.T # omega
+        # normalize -> softmax (sum 1) -> att weight*
+        attn_weights = torch.softmax(
+            attn_scores / keys.shape[-1]**0.5, dim=-1
+        )
+
+        # att weight @ values -> context
+        context_vec = attn_weights @ values
+        return context_vec
+
+torch.manual_seed(123)
+sa_v1 = SelfAttention_v1(d_in, d_out)
+
+
+
+
+
+# =============
+
+class SelfAttention_v2(nn.Module):
+
+    def __init__(self, d_in, d_out, qkv_bias=False):
+        super().__init__()
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key   = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+
+    def forward(self, x):
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        
+        attn_scores = queries @ keys.T
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+
+        context_vec = attn_weights @ values
+        return context_vec
+
+torch.manual_seed(789)
+sa_v2 = SelfAttention_v2(d_in, d_out)
+
+
+
+# =====
+
+# Reuse the query and key weight matrices of the
+# SelfAttention_v2 object from the previous section for convenience
+queries = sa_v2.W_query(inputs)
+keys = sa_v2.W_key(inputs) 
+attn_scores = queries @ keys.T
+print(attn_scores)
+
+attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+print(attn_weights)
+
+
+context_length = attn_scores.shape[0]
+mask_simple = torch.tril(torch.ones(context_length, context_length))
+print(mask_simple)
+
+
+masked_simple = attn_weights * mask_simple
+print(masked_simple)
