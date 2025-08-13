27e4faf657f9cfc5c837777cdc3372e8ab22abf4
Your Name <you@example.com>
2025-08-13 16:28:57 +1000

    WIP: test1.py

--- cmp_attn.py
+++ cmp_attn.py
@@ -4,7 +4,7 @@ import torch
 torch.manual_seed(123)
 # device cuda
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print(f"PyTorch version: {torch.__version__}")
+# print(f"PyTorch version: {torch.__version__}")
 
 # batch size 8
 batch_size = 8
@@ -21,3 +21,74 @@ embed_dim = 768
 # (8, 1024, 768)
 # basically single batch, single context len, has 768 dim
 embeddings = torch.randn((batch_size, context_len, embed_dim), device=device)
+
+
+# ==============
+
+import torch.nn as nn
+
+# casual attention
+# single head
+class CausalAttention(nn.Module):
+
+    # w_q, w_key, w_val
+    # dropout
+    # mask
+    # init
+    def __init__(self, d_in, d_out, context_length, dropout, qkv_bias=False):
+        super().__init__()
+        self.d_out = d_out
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer("mask", torch.triu(torch.ones(context_length, context_length), diagonal=1))  # New
+
+    # forward with attn score, attn weight, context
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape  # New batch dimension b
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+
+        attn_scores = queries @ keys.transpose(1, 2)  # Changed transpose
+        attn_scores.masked_fill_(  # New, _ ops are in-place
+            self.mask.bool()[:num_tokens, :num_tokens], -torch.inf)
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)  # New
+
+        context_vec = attn_weights @ values
+        return context_vec
+
+
+# multi head
+class Ch03_MHA_Wrapper(nn.Module):
+
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        self.heads = nn.ModuleList(
+            [CausalAttention(d_in, d_out, context_length, dropout, qkv_bias)
+             for _ in range(num_heads)]
+        )
+        self.out_proj = nn.Linear(d_out*num_heads, d_out*num_heads)
+
+    def forward(self, x):
+        # ok concat later
+        context_vec = torch.cat([head(x) for head in self.heads], dim=-1)
+        return self.out_proj(context_vec)
+
+
+mha_ch03_wrapper = Ch03_MHA_Wrapper(
+    # d_in, d_out tmp
+    # d_in = 768
+    d_in=embed_dim,
+    # 768 = 2^6 × 12 = 64 × 12; d_out = 64
+    d_out=embed_dim//12,
+    context_length=context_len,
+    dropout=0.0,
+    num_heads=12,
+    qkv_bias=False
+).to(device)
+
+out = mha_ch03_wrapper(embeddings)
+print(out.shape)
