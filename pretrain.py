c910e7b5da7dcb8549c3a90ed6896a19a38b9f01
Your Name <you@example.com>
2025-08-23 08:11:59 +1000

    WIP: update

--- pretrain.py
+++ pretrain.py
@@ -272,4 +272,40 @@ token_ids = generate_text_simple(
     context_size=GPT_CONFIG_124M["context_length"]
 )
 
-print("Output text:\n", token_ids_to_text(token_ids, tokenizer))
+
+# ================
+
+# input here
+inputs = torch.tensor([[16833, 3626, 6100],   # ["every effort moves",
+                       [40,    1107, 588]])   #  "I really like"]
+
+targets = torch.tensor([[3626, 6100, 345  ],  # [" effort moves you",
+                        [1107,  588, 11311]]) #  " really like chocolate"]
+
+
+with torch.no_grad():
+    logits = model(inputs)
+
+# probas Shape: (batch_size, num_tokens, vocab_size)
+probas = torch.softmax(logits, dim=-1) # Probability of each token in vocabulary
+
+
+token_ids = torch.argmax(probas, dim=-1, keepdim=True)
+
+
+
+text_idx = 0
+target_probas_1 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+print("Text 1:", target_probas_1)
+
+text_idx = 1
+target_probas_2 = probas[text_idx, [0, 1, 2], targets[text_idx]]
+print("Text 2:", target_probas_2)
+
+
+# Compute logarithm of all token probabilities
+log_probas = torch.log(torch.cat((target_probas_1, target_probas_2)))
+print(log_probas)
+
+avg_log_probas = torch.mean(log_probas)
+print(avg_log_probas)
